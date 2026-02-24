5cc3597c0a1aaf3b052aabc2113c82035ee36d08
Wouter Van Geluwe <vangeluw@adobe.com>
2026-02-25 06:10:09 +1100

    forms

--- scripts/editor-support.js
+++ scripts/editor-support.js
@@ -87,7 +87,9 @@ async function applyChanges(event) {
     if (block) {
       const blockResource = block.getAttribute('data-aue-resource');
       const newBlock = parsedUpdate.querySelector(`[data-aue-resource="${blockResource}"]`);
-      if (newBlock) {
+      if (block.dataset.aueModel === 'form') {
+        return true;
+      } else if (newBlock) {
         newBlock.style.display = 'none';
         block.insertAdjacentElement('afterend', newBlock);
         decorateButtons(newBlock);
@@ -130,7 +132,7 @@ async function applyChanges(event) {
   return false;
 }
 
-function attachEventListners(main) {
+async function attachEventListners(main) {
   [
     'aue:content-patch',
     'aue:content-update',
@@ -147,6 +149,8 @@ function attachEventListners(main) {
       window.location.reload();
     }
   }));
+  const module = await import('./form-editor-support.js');
+  module.attachEventListners(main);
 }
 
 attachEventListners(document.querySelector('main'));
